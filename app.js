917a599edb28a610abfcd9e6b6da062f3e8223e5
luca viegas <jsdeluca01@gmail.com>
2022-11-27 05:07:56 -0300

    Adding some animations

--- app.js
+++ app.js
@@ -4,8 +4,36 @@ const barsContainer = document.querySelector(".bar")
 const nav = document.querySelector("nav")
 
 
-barsContainer.addEventListener('click', ()=>{
+barsContainer.addEventListener('click', () => {
     barsContainer.classList.toggle("activeBars")
     nav.classList.toggle('activeNav')
 })
 
+
+
+//Animetions
+
+const fadeTopSimpleElements = document.querySelectorAll('[data-animation]')
+
+window.onscroll = () =>{
+
+    var scrollTop = window.pageYOffset + 600
+
+    fadeTopSimpleElements.forEach(element => {
+        if(scrollTop > element.offsetTop){
+            element.classList.add("anime")
+        }
+    })
+}
+
+
+
+
+
+//Change BigCard H3
+
+let bigCardH3 = document.querySelector('.bigContainer h3')
+
+if(window.innerWidth <= 800){
+    bigCardH3.innerHTML = "Tenha mais controle e facilidade para comprar seus salgados"
+}
